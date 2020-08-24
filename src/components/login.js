536b551a1e624cbc2929a9952c71cfd15cff3192
nayanr <nynrnjn@gmail.com>
2020-08-24 16:46:35 +0530

    Added Login Form

---
+++ src/components/login.js
@@ -0,0 +1,18 @@
+import React from "react";
+
+function LoginForm(){
+    return (
+    <div>
+    <label for="uname"><b>Username</b></label>
+    <input type="text" placeholder="Enter Username" name="uname" required />
+
+    <label for="psw"><b>Password</b></label>
+    <input type="password" placeholder="Enter Password" name="psw" required /> 
+
+    <button type="submit">Login</button>
+    </div>)
+}
+
+
+
+export default LoginForm;
